c611cc1d990b8fdfc6ff7b8e129a99aa46fa0dee
“Ariel <ariel50515@gmail.com>
2022-10-26 16:46:28 +0800

    20221026

---
+++ src/1.選單項目/Menu.js
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import './Menu.css'
+
+function Menu() {
+  const [activeText, setActiveText] = useState('')
+  const menuItems = ['首頁', '關於我們', '產品']
+  return (
+    <>
+      <ul>
+        {menuItems.map((v, i) => {
+          return (
+            <li
+              key={i}
+              onClick={() => {
+                setActiveText(v)
+              }}
+            >
+              <a href="#/" className={activeText === v ? 'activ' : ''}>
+                {v}
+              </a>
+            </li>
+          )
+        })}
+      </ul>
+    </>
+  )
+}
+
+export default Menu
